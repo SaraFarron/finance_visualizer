53bb08b1977b0db11d0dba93bdb3fbe699e27aec
Nikita Syulin <nikita_sulin@idpowers.com>
2022-04-21 13:49:48 +0700

    fix plots

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from utils import *
 
 FILE_NAME = 'operations Tue Dec 12 00_00_00 MSK 2017-Wed Apr 13 10_04_32 MSK 2022.csv'
-start = '01.07.2021 11:02:07'  # default 01.01.2017 00:00:00
-end = '20.04.2022 05:59:23'  # default 01.01.2023 00.00.00 or datetime.now() + timedelta(days=1)
+start = '01.01.2022 11:02:07'  # default 01.01.2017 00:00:00
+end = '01.04.2022 05:59:23'  # default 01.01.2023 00.00.00 or datetime.now() + timedelta(days=1)
 
 
 def main(start_date, end_date):
@@ -28,7 +28,9 @@ def main(start_date, end_date):
             monthly_data.append({
                 'start': shift,
                 'end': shift + timedelta(days=days),
-                'sum_of_all': 0
+                'income': 0,
+                'expenses': 0,
+                'profit': 0,
             })
             shift = shift + timedelta(days=days)
 
@@ -47,33 +49,38 @@ def main(start_date, end_date):
                         except KeyError:
                             month[t9n['Категория']] = 0
                             month[t9n['Категория']] += t9n_sum
+                        if t9n_sum > 0:
+                            month['income'] += t9n_sum
+                        else:
+                            month['expenses'] += t9n_sum
                 categories[t9n['Категория']] = []
     else:
         raise Exception('You need to provide both dates')
 
+    x = []
+    income, expenses, profit = [], [], []
     for month in monthly_data:
         for c, v in categories.items():
             try:
                 total = month[c]
             except KeyError:
                 total = 0
-            v.append(total)
-            month['sum_of_all'] += total
+            v.append(abs(total))
+        month['profit'] = month['income'] - abs(month['expenses'])
+        x.append(month['start'])
+        income.append(month['income'])
+        expenses.append(abs(month['expenses']))
+        profit.append(month['profit'])
 
-    # TODO THIS LINE
-
-    x = [x['start'] for x in monthly_data]
-    income, expenses, profit, income_categories, expense_categories = [], [], [], [], []  # TODO
-    for month in monthly_data:
-        s = month['sum_of_all']
-        if s < 0:
-            expenses.append(abs(s))
-            income.append(0)
-        else:
-            income.append(s)
-            expenses.append(0)
-        profit.append(income[-1] - expenses[-1])
-    labels = []
+    pie_expenses, pie_income = {}, {}
+    for k, v in monthly_data[-1].items():
+        if k == 'income' or k == 'profit' or k == 'expenses':
+            continue
+        if type(v) is float:
+            if v < 0:
+                pie_expenses[k] = abs(v)
+            else:
+                pie_income[k] = abs(v)
 
     # Plots
     fig, axs = plt.subplots(4)
@@ -81,20 +88,17 @@ def main(start_date, end_date):
     # Bar plots
     for c, v in categories.items():
         axs[0].bar(x, v, label=c)
-        labels.append(c)
     axs[0].plot(x, expenses)
     axs[0].grid()
     axs[0].legend()
 
-    # axs[1].pie(expenses, labels=labels)
-    # axs[2].pie(income, )  TODO
+    axs[1].pie(pie_expenses.values(), labels=pie_expenses.keys())
+    axs[2].pie(pie_income.values(), labels=pie_income.keys())
 
-    axs[2].bar(x, income, label='Income')
-    axs[2].bar(x, profit, label='Profit')
-    axs[2].grid()
-    axs[2].legend()
-
-    # axs[3].bar()  # TODO
+    axs[3].bar(x, income, label='Income')
+    axs[3].bar(x, profit, label='Profit')
+    axs[3].grid()
+    axs[3].legend()
 
     plt.show()
 
